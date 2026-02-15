9c0bd726ca55fb938e766f210208a3db88f11df4
o03793470-sketch <o03793470@gmail.com>
2026-02-15 17:44:35 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use bevy::prelude::*;
-use bevy_rapier3d::prelude::*;
 
-mod physics;
+mod netbin;
 mod scene;
 mod input;
 
+use netbin::{PhysicsBody, physics_step};
+
 fn main() {
     App::new()
+        .insert_resource(Vec::<PhysicsBody>::new())
         .add_plugins(DefaultPlugins)
-        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_startup_system(scene::setup)
-        .add_system(physics::physics_step)
+        .add_system(physics_system)
         .add_system(input::input_handling)
         .run();
+}
+
+fn physics_system(mut bodies: ResMut<Vec<PhysicsBody>>, time: Res<Time>, mut query: Query<&mut Transform>) {
+    physics_step(&mut bodies, time.delta_seconds());
+
+    // Оновлюємо трансформи в сцені
+    for (mut transform, body) in query.iter_mut().zip(bodies.iter()) {
+        *transform = body.transform;
+    }
 }
